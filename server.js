6389f6bbfc877e0a1faea436d3af51ea8dea38e4
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-02-06 03:15:46 +0200

    Server.js corrected

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api/v1/letters', letters);
 
 // Sever static assets if in production
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static(client/build));
+    app.use(express.static('client/build'));
     
     app.get('*', (req,res)=>{
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
