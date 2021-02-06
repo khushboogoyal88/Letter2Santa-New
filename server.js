310d7e55e09e6f2607a58d1ad3ebf47e2e0023e5
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-02-06 03:46:12 +0200

    Trying...

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const path = require('path');
