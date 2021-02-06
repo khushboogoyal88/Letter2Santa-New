bcdb70ebf31a30dbb88ac565cd1eb96a5f904cab
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-02-06 02:39:08 +0200

    Version 1.0.0

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const path = require('path');
 const connectDB = require('./config/db')
-
 
 //DB config 
 dotenv.config({ path: './config/config.env' });
@@ -18,6 +18,15 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1/letters', letters);
+
+// Sever static assets if in production
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(client/build));
+    
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 // Bodyparser middleware
 app.use(bodyParser.json());
